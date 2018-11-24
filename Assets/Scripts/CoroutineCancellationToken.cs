f3046352c33bdf0d8d88073926c5c0ccb19500f7
NFetissow <nfetissow@gmail.com>
2018-11-24 15:19:00 +0100

    Added a comment for CoroutineCancellationToken class

--- Assets/Scripts/CoroutineCancellationToken.cs
+++ Assets/Scripts/CoroutineCancellationToken.cs
@@ -1,5 +1,8 @@
 ﻿namespace MastersOfTempest
 {
+    /// <summary>
+    /// Pass this as an argument to coroutines that you want to cancel from the outside
+    /// </summary>
     public class CoroutineCancellationToken
     {
         public bool CancellationRequested { get; set; } = false;
