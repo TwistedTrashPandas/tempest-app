0174840471cbb631be2e256d8696504223884457
NFetissow <nfetissow@gmail.com>
2018-12-04 15:53:51 +0100

    Updated XML comment for UIManager

--- Assets/Scripts/Player/UI/UIManager.cs
+++ Assets/Scripts/Player/UI/UIManager.cs
@@ -47,7 +47,7 @@ namespace MastersOfTempest.PlayerControls
         /// Instantiates given prefab and attaches it to the canvas
         /// </summary>
         /// <param name="prefab">Prefab to instantiate</param>
-        /// <typeparam name="T">MonoBehaviour with attached RectTransform</typeparam>
+        /// <typeparam name="T"><see cref="MonoBehaviour"/> with attached RectTransform</typeparam>
         /// <returns>Returns instance</returns>
         public T SpawnUIElement<T>(T prefab) where T : MonoBehaviour
         {
@@ -60,7 +60,7 @@ namespace MastersOfTempest.PlayerControls
         /// Loads prefab from resources and instantiates it on the Canvas
         /// </summary>
         /// <param name="path">Path to the prefab in the Resources folder</param>
-        /// <typeparam name="T">MonoBehaviour with RectTransform</typeparam>
+        /// <typeparam name="T"><see cref="MonoBehaviour"/> with RectTransform</typeparam>
         /// <returns>Returns instance</returns>
         public T SpawnUIElement<T>(string path) where T : MonoBehaviour
         {
