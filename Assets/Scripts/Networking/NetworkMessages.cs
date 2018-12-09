30b14d1ffe9e617632ea69fb70558825aa74d241
Moritz <momower1@hotmail.de>
2018-12-09 15:10:34 +0100

    Removed no longer necessary NetworkMessageTypes

--- Assets/Scripts/Networking/NetworkMessages.cs
+++ Assets/Scripts/Networking/NetworkMessages.cs
@@ -1,22 +1,15 @@
 ﻿namespace MastersOfTempest.Networking
 {
-    // Add new types here (e.g. the name of the class that uses it)
-    // Make sure to assign and never change the index
     public enum NetworkMessageType
     {
-        Empty = 0,
-        LobbyChat = 1,
-        LobbyStartGame = 2,
-        ServerObject = 3,
-        DestroyServerObject = 4,
-        NetworkBehaviourInitialized = 5,
-        PingPong = 6,
-        ForceManipulator = 7,
-        NoMessages = 8,
-        EnvObjects = 9,
-        ServerObjectList = 10,
-        ClientReadyForInitialization = 11,
-        CubeNetworkBehaviour = 12,
-        NetworkBehaviour = 13
+        PingPong,
+        LobbyChat,
+        LobbyStartGame,
+        ServerObject,
+        ServerObjectList,
+        DestroyServerObject,
+        NetworkBehaviour,
+        NetworkBehaviourInitialized,
+        ClientReadyForInitialization
     };
 }
