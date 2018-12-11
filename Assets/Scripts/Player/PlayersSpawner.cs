45f733f6174cac68036c236a9e4233aaef9e62b4
NFetissow <nfetissow@gmail.com>
2018-12-11 20:42:30 +0100

    Spawning player for each player Now PlayersSpawner takes care of spawning Player instances for each participating party. The Player class now uses more of the networking to support that.

---
+++ Assets/Scripts/Player/PlayersSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MastersOfTempest.Networking;
+
+namespace MastersOfTempest.PlayerControls
+{
+    /// <summary>
+    /// Spawns the player object for each player from the Lobby and then terminates self
+    /// </summary>
+    public class PlayersSpawner : MonoBehaviour
+    {
+        public Player PlayerPrefab;
+        private void Awake()
+        {
+            if (PlayerPrefab == null)
+            {
+                throw new InvalidOperationException($"{nameof(PlayerPrefab)} is not specified!");
+            }
+
+            var players = NetworkManager.Instance.GetLobbyMemberIDs();
+            foreach(var playerId in players)
+            {
+                //Set parent so that it goes to the proper scene
+                var playerInstance = Instantiate(PlayerPrefab, this.transform);
+                playerInstance.PlayerId = playerId;
+                //Remove the parent so that we are not destroyed with the spawner
+                playerInstance.transform.parent = null;
+                Debug.Log($"Spawned for player# {playerId}");
+            }
+            //We don't need it anymore after its job is done
+            Destroy(this.gameObject);
+        }
+    }
+}
