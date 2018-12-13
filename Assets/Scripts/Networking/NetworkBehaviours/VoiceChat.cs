f6a0a3c1b17a784170c307b703254ca1f0f8843b
Moritz <momower1@hotmail.de>
2018-12-13 16:30:22 +0100

    Fixed voice chat issue where you would not hear the other person

--- Assets/Scripts/Networking/NetworkBehaviours/VoiceChat.cs
+++ Assets/Scripts/Networking/NetworkBehaviours/VoiceChat.cs
@@ -9,7 +9,7 @@ namespace MastersOfTempest.Networking
     [RequireComponent(typeof(AudioSource))]
     public class VoiceChat : NetworkBehaviour
     {
-        public bool hearYourself = true;
+        public bool hearYourself = false;
 
         private AudioSource audioSource;
 
@@ -26,37 +26,42 @@ namespace MastersOfTempest.Networking
             byte[] compressedData = new byte[dataLength];
             Array.Copy(data, compressedData, dataLength);
 
-            SendToServer(compressedData, Facepunch.Steamworks.Networking.SendType.Reliable);
+            // Send to all clients but yourself (if you don't want to hear yourself)
+            ulong[] memberIDs = NetworkManager.Instance.GetLobbyMemberIDs();
+
+            foreach (ulong steamID in memberIDs)
+            {
+                if (hearYourself || steamID != Facepunch.Steamworks.Client.Instance.SteamId)
+                {
+                    SendToClient(steamID, compressedData, Facepunch.Steamworks.Networking.SendType.Reliable);
+                }
+            }
         }
 
         protected override void OnClientReceivedMessageRaw(byte[] data, ulong steamID)
         {
-            // You should not be able to hear yourself
-            if (hearYourself || Facepunch.Steamworks.Client.Instance.SteamId != steamID)
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+
+            if (Facepunch.Steamworks.Client.Instance.Voice.Decompress(data, stream))
             {
-                System.IO.MemoryStream stream = new System.IO.MemoryStream();
+                // 16 bit signed PCM data
+                byte[] uncompressedData = stream.ToArray();
 
-                if (Facepunch.Steamworks.Client.Instance.Voice.Decompress(data, stream))
+                float[] samples = new float[uncompressedData.Length / 2];
+
+                for (int i = 0; i < uncompressedData.Length; i += 2)
                 {
-                    // 16 bit signed PCM data
-                    byte[] uncompressedData = stream.ToArray();
-
-                    float[] samples = new float[uncompressedData.Length / 2];
-
-                    for (int i = 0; i < uncompressedData.Length; i += 2)
-                    {
-                        samples[i / 2] = (BitConverter.ToInt16(uncompressedData, i) / (float)Int16.MaxValue);
-                    }
-
-                    // TODO: Use streaming feature instead of this
-                    AudioClip clip = AudioClip.Create("Voice Chat", samples.Length, 1, (int)Facepunch.Steamworks.Client.Instance.Voice.OptimalSampleRate, false);
-                    clip.SetData(samples, 0);
-                    audioSource.PlayOneShot(clip);
+                    samples[i / 2] = (BitConverter.ToInt16(uncompressedData, i) / (float)Int16.MaxValue);
                 }
-                else
-                {
-                    Debug.LogError("Failed to decompress Voice Chat data!");
-                }
+
+                // TODO: Use streaming feature instead of this
+                AudioClip clip = AudioClip.Create("Voice Chat", samples.Length, 1, (int)Facepunch.Steamworks.Client.Instance.Voice.OptimalSampleRate, false);
+                clip.SetData(samples, 0);
+                audioSource.PlayOneShot(clip);
+            }
+            else
+            {
+                Debug.LogError("Failed to decompress Voice Chat data!");
             }
         }
 
@@ -67,7 +72,10 @@ namespace MastersOfTempest.Networking
 
         protected override void OnDestroyClient()
         {
-            Facepunch.Steamworks.Client.Instance.Voice.OnCompressedData -= OnCompressedData;
+            if (Facepunch.Steamworks.Client.Instance != null)
+            {
+                Facepunch.Steamworks.Client.Instance.Voice.OnCompressedData -= OnCompressedData;
+            }
         }
     }
 }
