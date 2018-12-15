d0b856ea621987dfaf2530f2d921beea452bc75a
Moritz <momower1@hotmail.de>
2018-12-15 19:35:56 +0100

    Hologram now works in play mode

---
+++ Assets/Scripts/Hologram/Hologram.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Hologram : MonoBehaviour
+{
+    public Transform target;
+    public Camera hologramCamera;
+
+    public float distance = 20;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update ()
+    {
+        transform.LookAt(mainCamera.transform);
+
+        Vector3 viewingDirection = (mainCamera.transform.position - transform.position).normalized;
+        hologramCamera.transform.position = target.position + distance * viewingDirection;
+        hologramCamera.transform.LookAt(target);
+	}
+}
