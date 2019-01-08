c95ba8a2a120ec307831c0e027a804c99c1bcda8
JPavlova <jenia_pavlova@gmx.de>
2019-01-08 23:50:20 +0100

    WizardAnimationScript
    Outcommned

--- Assets/Scripts/Player/PlayerControls/WizardInputAnimations.cs
+++ Assets/Scripts/Player/PlayerControls/WizardInputAnimations.cs
@@ -8,7 +8,7 @@ namespace MastersOfTempest.PlayerControls
     public class WizardInputAnimations : MonoBehaviour
     {
         public WizardInput WizardInput;
-        public Animator anim; 
+        //public Animator anim; 
 
         private void Start()
         {
@@ -22,44 +22,44 @@ namespace MastersOfTempest.PlayerControls
             WizardInput.DischargeHit += OnDischargeHit;
             WizardInput.DischargeMiss += OnDischargeMiss;
 
-            anim = GetComponent<Animator>();
-            anim.Play("Armature | Idle");
+            //anim = GetComponent<Animator>();
+            //anim.Play("Armature | Idle");
             
         }
 
         private void OnChargeStarted(object sender, EventArgs args)
         {
             
-            anim.CrossFade("Armature|GrabMagicRight", 0.5f);
-            anim.CrossFade("Armature|RightHandGrab", 0.5f);
-            anim.CrossFade("Armature|RightHandHold", 0.1f);
+            //anim.CrossFade("Armature|GrabMagicRight", 0.5f);
+            //anim.CrossFade("Armature|RightHandGrab", 0.5f);
+            //anim.CrossFade("Armature|RightHandHold", 0.1f);
             Debug.Log("Animation for charge starting showed");
         }
 
         private void OnChargeCancelled(object sender, EventArgs args)
         {
             
-            anim.CrossFade("Armature|Idle", 0.5f);
+            //anim.CrossFade("Armature|Idle", 0.5f);
             Debug.Log("Animation for charge cancelled showed");   
         }
 
         private void OnChargeCompleted(object sender, EventArgs args)
         {
             
-            anim.CrossFade("Armature|Idle", 0.5f);
+            //anim.CrossFade("Armature|Idle", 0.5f);
             Debug.Log("Animation for charge completion showed");
         }
 
         private void OnDischargeHit(object sender, EventArgs args)
         {
-            anim.CrossFade("Armature|RightHandGrab", 0.5f);
+            //anim.CrossFade("Armature|RightHandGrab", 0.5f);
             Debug.Log("Animation for discharge hit showed");
         }
 
         private void OnDischargeMiss(object sender, EventArgs args)
         {
 
-            anim.CrossFade("Armature|RightHandGrab", 0.5f); //Placeholder Animation atm
+            //anim.CrossFade("Armature|RightHandGrab", 0.5f); //Placeholder Animation atm
             Debug.Log("Animation for discharge miss showed");   
         }
     }
