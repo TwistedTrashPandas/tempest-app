66bb66ba67104257831e3e483ca1a8ef8ad5d7e8
JPavlova <jenia_pavlova@gmx.de>
2019-01-09 19:10:47 +0100

    Work on Wizard/Rock Animations

--- Assets/Scripts/Environment/Objects/RockAnimator.cs
+++ Assets/Scripts/Environment/Objects/RockAnimator.cs
@@ -11,13 +11,14 @@ public class RockAnimator : MonoBehaviour
 
     public bool isDestroyed = false;
     public bool init = false;
-    public float force = 3f;
-    public float rotSpeed = 2.5f;
+    public float force = 5.0f;
+    public float rotSpeed = 0.3f;
     private Vector3[] randomDir = new Vector3[5] ;
     private Quaternion[] randomRot = new Quaternion[5];
     
     void Start()
     {
+        /*Already done in Damaging.cs
         parent = gameObject.GetComponentInParent<Transform>().gameObject;
 
         foreach (Transform child in transform)
@@ -27,24 +28,20 @@ public class RockAnimator : MonoBehaviour
 
        
         children.ToArray();
+        */
+
+        InitValues();
     }
 
     
     void Update()
     {
-        if(Input.GetKeyDown("k"))
-        {
-            InitValues();
-            isDestroyed = true;
-        }
-        if(isDestroyed == true)
-        {
-            SplitRock();
-        }
+            SplitRock();    
     }
 
     private void InitValues()
     {
+        /* Already done in Damaging.cs
         Debug.Log("initValue");
         foreach(GameObject a in children)
         {
@@ -52,9 +49,7 @@ public class RockAnimator : MonoBehaviour
         }
        
         children.Add(parent);
-
-        Debug.Log(children.Count);
-        Debug.Log(randomDir.Length);
+        */
 
         randomDir[0] = new Vector3(Random.value, Random.value, Random.value);
         randomDir[1] = new Vector3(randomDir[0].x * (-1.0f), randomDir[0].y * (-1.0f), randomDir[0].z * (-1.0f)) ;
@@ -64,21 +59,15 @@ public class RockAnimator : MonoBehaviour
         randomRot[1] = Quaternion.Euler(new Vector3(Random.Range(-180.0f, 180.0f), Random.Range(-180.0f, 180.0f), Random.Range(-180.0f, 180.0f)));
         randomRot[2] = Quaternion.Euler(new Vector3(Random.Range(-180.0f, 180.0f), Random.Range(-180.0f, 180.0f), Random.Range(-180.0f, 180.0f)));
 
-
-        Debug.Log(randomDir.Length);
         init = true;
     }
 
     private void SplitRock()
     {
-        Debug.Log("I'm here!");
-        Debug.Log(children[0].name);
-        Debug.Log(children[1].name);
         int j = 0;
         foreach (GameObject a in children)
         {
             a.transform.Translate(randomDir[j] * Time.deltaTime * force);
-            // a.gameObject.transform.rotation = Quaternion.Slerp(transform.rotation, randomRot[j], Time.deltaTime * rotSpeed);
             a.transform.Rotate(randomRot[j].eulerAngles*Time.deltaTime*rotSpeed);
             j++;
         }
---
+++ Assets/Scripts/Player/PlayerControls/WizardKeybindAnimations.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardKeybindAnimations : MonoBehaviour
+{
+
+    /*
+     * Explaination 
+     *  ANIMATIONS: 
+     *  - Armature|Idle; Armature|IdleLeft; Armature|IdleRight : selfexplaining, also used to reset the animationloops in the animator
+     *  - Armature|GrabBook; Armature|HoldBook; Armature|StowBook : Animationstates of the Book-Loop
+     *  - Armature|RightHandGrab; Armature|RightHandHold; Armature|RightHandStow : Animationstates of the Spell-Holding-Loop
+     *  - Armature|HandRightPulse : This animation shall occure every time the Spell fails charging or, does not hit the target. But only if the wizards still holds on to the energy afterwards.
+     *  
+     *  PARAMETERS
+     *  bool holdBook : true -> enter HoldAnimation; false -> end HoldAnimation
+     *  bool holdSpell : true -> enter RightHandHoldAnimation; false -> end RightHandHoldAnimation
+     *  trigger pulse : "trigger" works like a onetime bool. It will let the animation "Pulse" loop one time, bevor leaving it again
+     * */
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("Armature|Idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("q"))
+        {
+            anim.SetBool("holdBook",true);
+            anim.CrossFade("Armature|GrabBook", 0.5f);
+        }
+        if(Input.GetKeyDown("w"))
+        {
+            anim.SetBool("holdBook", false);
+
+        }
+        if(Input.GetKeyDown("e"))
+        {
+            anim.SetBool("holdSpell", true);
+            anim.CrossFade("Armature|RightHandGrab", 0.5f);
+        }
+        if(Input.GetKeyDown("t"))
+        {
+            anim.SetTrigger("pulse");
+        }
+        if(Input.GetKeyDown("r"))
+        {
+            anim.SetBool("holdSpell", false);
+        }
+    }
+}
