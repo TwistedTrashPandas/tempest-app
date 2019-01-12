c32b85a2f96909dac3eb68494b13c43b0d66692d
Moritz <22204103+momower1@users.noreply.github.com>
2019-01-12 23:03:11 +0100

    Added NetworkBehaviour to handle the teleportation
    Also a returnTranform will be used to set the player position when you want to teleport back

---
+++ Assets/Scripts/Player/Actions/TeleportActionNetworked.cs
@@ -0,0 +1,9 @@
+﻿using MastersOfTempest.Networking;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportActionNetworked : NetworkBehaviour
+{
+
+}
