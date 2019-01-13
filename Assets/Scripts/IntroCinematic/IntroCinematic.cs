fc794192e5f8f50a0cbf90ffd81e243ef31f3091
Moritz <22204103+momower1@users.noreply.github.com>
2019-01-13 15:57:26 +0100

    Simple IntroCinematic added

---
+++ Assets/Scripts/IntroCinematic/IntroCinematic.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using Facepunch.Steamworks;
+using MastersOfTempest.ShipBL;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroCinematic : MonoBehaviour
+{
+    public CinemachineVirtualCamera[] cinemachineVirtualCameras;
+    public float timeUntilLevelLoad = 5;
+    public string clientSceneName = "Client";
+    public string serverSceneName = "Server";
+
+    public void Configure ()
+    {
+        Transform ship = FindObjectOfType<Ship>().transform;
+
+        foreach (CinemachineVirtualCamera c in cinemachineVirtualCameras)
+        {
+            c.LookAt = ship;
+            c.Follow = ship;
+        }
+
+        StartCoroutine(LoadLevel());
+    }
+
+    private IEnumerator LoadLevel ()
+    {
+        yield return new WaitForSeconds(timeUntilLevelLoad);
+
+        // Load client scene
+        SceneManager.LoadScene(clientSceneName);
+
+        // Also load server scene if you are the owner of the lobby
+        if (Client.Instance.Lobby.Owner == Client.Instance.SteamId)
+        {
+            SceneManager.LoadScene(serverSceneName, LoadSceneMode.Additive);
+        }
+    }
+}
