fdc92e992cd6c5b3f18d9ef5184ed4eb00748950
NFetissow <nfetissow@gmail.com>
2019-01-14 17:36:50 +0100

    Added up and down spells

---
+++ Assets/Scripts/Player/PlayerControls/Spellcasting/Spells/SteerDownSpell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MastersOfTempest.PlayerControls.Spellcasting
+{
+    public class SteerDownSpell : Spell
+    {
+        public override Charge[] SpellSequence
+        {
+            get
+            {
+                return new Charge[] {Charge.Water, Charge.Fire, Charge.Earth, Charge.Earth};
+            }
+        }
+
+        public override String Name
+        {
+            get
+            {
+                return "Down!";
+            }
+        }
+
+        public override PlayerAction GetPlayerAction()
+        {
+            return new SteerShip(SteerShip.SteeringDirection.Down);
+        }
+    }
+}
