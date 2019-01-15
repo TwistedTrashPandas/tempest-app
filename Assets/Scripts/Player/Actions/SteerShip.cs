cbd11e2574b1ab6870df42ff45f7a898b22597ce
NFetissow <nfetissow@gmail.com>
2019-01-16 00:37:17 +0100

    Adjusted the ship's speed

--- Assets/Scripts/Player/Actions/SteerShip.cs
+++ Assets/Scripts/Player/Actions/SteerShip.cs
@@ -5,7 +5,7 @@ namespace MastersOfTempest.PlayerControls
 {
     public class SteerShip : PlayerAction
     {
-        const float SteeringForceValue = 100000f;
+        const float SteeringForceValue = 20000f;
         public enum SteeringDirection
         {
             Left,
