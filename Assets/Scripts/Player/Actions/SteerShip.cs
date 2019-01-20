a1efdc600ab445be38b068b08f3074d03a6b0353
Maximilian Werhahn <maxi.werhahn@gmx.de>
2019-01-20 21:11:47 +0100

    increased steering force, fixed right/left mixup, added trail renderer

--- Assets/Scripts/Player/Actions/SteerShip.cs
+++ Assets/Scripts/Player/Actions/SteerShip.cs
@@ -5,7 +5,7 @@ namespace MastersOfTempest.PlayerControls
 {
     public class SteerShip : PlayerAction
     {
-        const float SteeringForceValue = 1000f;
+        const float SteeringForceValue = 2500f;
         public enum SteeringDirection
         {
             Left,
@@ -27,15 +27,15 @@ namespace MastersOfTempest.PlayerControls
             var ship = context.GetShip();
             switch (direction)
             {
-                case SteeringDirection.Left: forceDirection = ship.transform.right; break;
-                case SteeringDirection.Right: forceDirection = -ship.transform.right; break;
+                case SteeringDirection.Left: forceDirection = -ship.transform.right; break;
+                case SteeringDirection.Right: forceDirection = ship.transform.right; break;
                 case SteeringDirection.Forward: forceDirection = ship.transform.forward; break;
                 case SteeringDirection.Backward: forceDirection = -ship.transform.forward; break;
                 case SteeringDirection.Up: forceDirection = ship.transform.up; break;
                 case SteeringDirection.Down: forceDirection = -ship.transform.up; break;
                 default: throw new InvalidOperationException($"Unknown value {nameof(SteeringDirection)} of {direction}");
             }
-
+            Debug.Log("Direction: " + forceDirection);
             //TODO: duration for the force, or add as an impulse
             if ((ship.GetCurrenStatus().Condition & ShipBL.ShipCondition.Freezing) == ShipBL.ShipCondition.Freezing)
                 forceDirection *= ship.GetFreezingSlowDown();
