13c2999b39d343de0e5e7b881a9c785bc2963f96
Moritz <22204103+momower1@users.noreply.github.com>
2019-01-20 13:14:16 +0100

    Added ESC screen to Lobby scene

---
+++ Assets/Scripts/UI/ESCScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ESCScreen : MonoBehaviour
+{
+    public static ESCScreen Instance = null;
+
+    public KeyCode keyToPress = KeyCode.Escape;
+
+    private Canvas canvas;
+
+    protected void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected void Start()
+    {
+        canvas = GetComponent<Canvas>();
+    }
+
+    protected void Update()
+    {
+        if (Input.GetKeyDown(keyToPress))
+        {
+            Toggle();
+        }
+    }
+
+    public void LoadScene (string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Toggle()
+    {
+        canvas.enabled = !canvas.enabled;
+    }
+}
