92fa7d9d40d3f80d2912ff5f007c37899f926ca4
JPavlova <jenia_pavlova@gmx.de>
2019-02-02 20:30:53 +0100

    Last Tweaks for the Tutorial

--- Assets/TutorialBookHandler.cs
+++ Assets/TutorialBookHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace MastersOfTempest
 {
@@ -39,7 +40,14 @@ namespace MastersOfTempest
         {
             spellbook.PreviousPage();
         }
-    }
+
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+                SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+        }
+
+        }
     }
 
 }
